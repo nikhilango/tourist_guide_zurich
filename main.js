726a18d351620179c7a39ff71b3ea345d61f97d1
nikhilango <nikhil.ilango@noseryoung.com>
2022-10-14 10:46:32 +0200

    made the photos more responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+var toggleButton = document.getElementsByClassName('toggle-button')[0]
+var navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
